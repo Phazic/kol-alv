0c79f825178d1ea93ea66ea62a2e766946fdd546
bombar <bombarkol@gmail.com>
2015-02-23 17:03:49 -0500

    Add Banisher

---
+++ src/com/googlecode/logVisualizer/parser/lineParsers/MafiaBanishLineParser.java
@@ -0,0 +1,54 @@
+/* Copyright (c) 2008-2011, developers of the Ascension Log Visualizer
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.googlecode.logVisualizer.parser.lineParsers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.googlecode.logVisualizer.logData.LogDataHolder;
+import com.googlecode.logVisualizer.logData.turn.SingleTurn;
+import com.googlecode.logVisualizer.parser.UsefulPatterns;
+
+public final class MafiaBanishLineParser extends AbstractLineParser {
+	
+    private static final String LOUDER_THAN_BOMB_USAGE_STRING = " uses the Louder Than Bomb";
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doParsing(
+                             final String line, final LogDataHolder logData) {
+        ((SingleTurn) logData.getLastTurnSpent()).setBanished(true);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean isCompatibleLine(final String line) {
+    	return line.contains( LOUDER_THAN_BOMB_USAGE_STRING );
+    }
+}
