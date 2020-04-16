700a2cea1902cd67cedb4b3052f30a8438269220
Mark Meyer <reyemkram@verizon.net>
2020-04-16 13:39:07 -0500

    Fill in missing Javadocs

--- src/com/googlecode/alv/creator/HTMLLogCreator.java
+++ src/com/googlecode/alv/creator/HTMLLogCreator.java
@@ -1,3 +1,27 @@
+/* Copyright (c) 2020-2020, developers of the Ascension Log Visualizer
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.googlecode.alv.creator;
 
 import java.util.Collections;
@@ -6,13 +30,28 @@ import com.googlecode.alv.logData.LogDataHolder;
 import com.googlecode.alv.logData.Skill;
 import com.googlecode.alv.logData.turn.turnAction.DayChange;
 
+/**
+ * This class extends TextLogCreator so as to create HTML log output.  It 
+ * overrides several of TextLogCreator's output methods to take advantage 
+ * of HTML's features.
+ */
 public class HTMLLogCreator extends TextLogCreator {
 
+    /**
+     * Creates an HTMLLogCreator instance for further use.
+     *
+     * @param logData
+     *            The ascension log data from which the parsed ascension log
+     *            should be created.
+     */
     public HTMLLogCreator(LogDataHolder logData) {
         super(logData);
         // Nothing else to do for this class
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void setAugmentationsMap()
     {
@@ -37,18 +76,30 @@ public class HTMLLogCreator extends TextLogCreator {
         writeln("<body>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printTitle(final LogDataHolder logData, final int ascensionStartDate)
     {
         writeln("<h1>NEW " + logData.getCharacterClass() + " " + logData.getGameMode() + " "
                 + logData.getAscensionPath() + " ASCENSION STARTED " + ascensionStartDate + "</h1>");
     }
-    
+
+    /**
+     * Print a line in the table of contents.  
+     * 
+     * @param text String, usually a section name, that should appear in the table of contents.
+     * @param anchor Name of the HTML anchor to which to link the TOC entry.
+     */
     protected void printTOCLine(String text, String anchor)
     {
         writeln("<br><a href=\"#" + anchor + "\">" + text + "</a>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printTableOfContents(LogDataHolder logData)
     {
@@ -82,18 +133,27 @@ public class HTMLLogCreator extends TextLogCreator {
         writeln("</td></tr></table>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printSectionHeader(String title, String anchor)
     {
         writeln("<a name=\"" + anchor + "\"/><h2>" + title + "</h2>");
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printTableStart()
     {
         writeln("<table>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printTableRow(String ...strings)
     {
@@ -106,6 +166,9 @@ public class HTMLLogCreator extends TextLogCreator {
         writeln("</tr>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printTableEnd()
     {
@@ -129,6 +192,9 @@ public class HTMLLogCreator extends TextLogCreator {
         writelnWithBreak();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printDayChange(DayChange nextDay) 
     {
@@ -137,18 +203,27 @@ public class HTMLLogCreator extends TextLogCreator {
         writeln(logAdditionsMap.get("dayChangeLineEnd"));
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printParagraphStart()
     {
         write("<p>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void printParagraphEnd()
     {
         write("</p>");
     }
     
+    /**
+     * {@inheritDoc}
+     */
     protected void printLineBreak()
     {
         write("<br>");
